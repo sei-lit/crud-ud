af4fc4fba6f096b52446aac913f3d0e64d36167f
Hayato1031 <nakamurahayato1031@keio.jp>
2024-06-27 19:10:59 +0900

    feat:update,delete

--- app.rb
+++ app.rb
@@ -8,11 +8,30 @@ get '/' do
   erb:index
 end
 
-get '/medicines/new' do
+get '/item/create' do
   erb :new
 end
 
-post '/medicines/new' do
+post '/item/create' do
   Medicine.create(name: params[:name], stock: params[:stock])
   redirect '/'
+end
+
+get '/item/:item_id' do
+  @item = Medicine.find(params[:item_id])
+  erb :item
+end
+
+post '/item/:item_id' do
+  item = Medicine.find(params[:item_id])
+  item.name = params[:item_name]
+  item.stock = params[:item_stock]
+  item.save
+  redirect "/item/#{item.id}"
+end
+
+get '/item/:item_id/delete' do
+  item = Medicine.find(params[:item_id])
+  item.destroy
+  redirect "/"
 end
